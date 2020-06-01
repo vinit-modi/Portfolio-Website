eb5955fd533a78dce85f588af90892e16ba07b8c
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-06-01 21:52:03 +0530

    scrolling functionality added on "about" and logo

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,11 @@
-$("nav-item about-button").click(function() {
+$(".about-button").click(function() {
     $('html,body').animate({
         scrollTop: $(".about-area").offset().top},
+        'slow');
+});
+
+$(".footer-logo").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".header_area").offset().top},
         'slow');
 });
