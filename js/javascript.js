fc7c9d94db78e7437d1bac0d9e153cb58f3265da
vinit-modi <vinitmodi.dev@gmail.com>
2023-11-29 03:57:43 -0500

    My project section added

--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,44 @@
-$(".about-button").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".about-area").offset().top},
-        'slow');
+$(".about-button").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".about-area").offset().top,
+    },
+    "slow"
+  );
 });
 
-$(".footer-logo").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".header_area").offset().top},
-        'slow');
+$(".footer-logo").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".header_area").offset().top,
+    },
+    "slow"
+  );
 });
 
-$(document).ready(function(){
-     $(".footer-logo, .navbar-brand").mouseover(function(){
-         $(".logo-class").css("color", "#854fee");
-     });
+$(".project-button").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".projects-area").offset().top,
+    },
+    "slow"
+  );
 });
 
-$(document).ready(function(){
-    $(".footer-logo, .navbar-brand").mouseout(function(){
-        $(".logo-class").css("color", "black");
-    });
-});
-
-$(function() {
-    $(document).click(function (event) {
-      $('.navbar-collapse').collapse('hide');
-    });
+$(document).ready(function () {
+  $(".footer-logo, .navbar-brand").mouseover(function () {
+    $(".logo-class").css("color", "#854fee");
   });
+});
+
+$(document).ready(function () {
+  $(".footer-logo, .navbar-brand").mouseout(function () {
+    $(".logo-class").css("color", "black");
+  });
+});
+
+$(function () {
+  $(document).click(function (event) {
+    $(".navbar-collapse").collapse("hide");
+  });
+});
